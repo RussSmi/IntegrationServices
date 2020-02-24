9b01338078aec3bc34c49f4e5dc1537f720556aa
Russell Smith <44894904+RussSmi@users.noreply.github.com>
2020-02-24 20:49:27 +0000

    Update Demo.tf

--- Demo.tf
+++ Demo.tf
@@ -53,7 +53,7 @@ resource "azurerm_cosmosdb_sql_container" "demo" {
   resource_group_name = azurerm_cosmosdb_account.demo.resource_group_name
   account_name        = azurerm_cosmosdb_account.demo.name
   database_name       = azurerm_cosmosdb_sql_database.demo.name
-  throughput          = 400
+  /* throughput          = 400 */
 
 }
 
