f8e8a659d5c0a19f3eb34b81f078b9364eb2aafe
Russell Smith <44894904+RussSmi@users.noreply.github.com>
2020-03-03 17:03:51 +0000

    Update get-logic-app-url.sh

--- apim/bashjson/get-logic-app-url.sh
+++ apim/bashjson/get-logic-app-url.sh
@@ -2,8 +2,8 @@
 az rest --method post --uri https://management.azure.com/subscriptions/#{subscription-id}#/resourceGroups/rg-is-demo/providers/Microsoft.Logic/workflows/la-is-demo-receive/triggers/manual/listCallbackUrl?api-version=2016-10-01 > la-is-demo-receive.json
 laurl=./bashjson.sh la-is-demo-receive.json basePath
 lasig=./bashjson.sh la-is-demo-receive.json queries sig
-laurltrim=${la-url%/manual/paths/invoke}
-echo "##vso[task.setvariable=SERVICEURL]$(laurltrim)"
-echo "##vso[task.setvariable=SIG]$(lasig)"
+#laurltrim=${la-url%/manual/paths/invoke}
+echo "##vso[task.setvariable=SERVICEURL]"${la-url%/manual/paths/invoke}
+echo "##vso[task.setvariable=SIG]"$lasig
 
 
