d49629a7815d897c9c34c93109645a48b441f56b
RussSmi <russell_smith@hotmail.com>
2020-03-04 15:42:38 +0000

    Changed vso to export

--- apim/bashjson/get-logic-app-url.sh
+++ apim/bashjson/get-logic-app-url.sh
@@ -1,7 +1,7 @@
 # This script needs to run in the bashjson folder
 az rest --method post --uri https://management.azure.com/subscriptions/#{subscription-id}#/resourceGroups/rg-is-demo/providers/Microsoft.Logic/workflows/la-is-demo-receive/triggers/manual/listCallbackUrl?api-version=2016-10-01 > la-is-demo-receive.json
-LAURL=$(./bashjson.sh la-is-demo-receive.json basePath)
-LASIG=$(./bashjson.sh la-is-demo-receive.json queries sig)
-LAURLTRIM=${LAURL%/manual/paths/invoke}
-echo ##vso[task.setvariable=SERVICEURL]$LAURLTRIM
-echo ##vso[task.setvariable=SIG]$LASIG
+export LAURL=$(./bashjson.sh la-is-demo-receive.json basePath)
+export SIG=$(./bashjson.sh la-is-demo-receive.json queries sig)
+export SERVICEURL=${LAURL%/manual/paths/invoke}
+#echo ##vso[task.setvariable=SERVICEURL]$LAURLTRIM
+#echo ##vso[task.setvariable=SIG]$LASIG
